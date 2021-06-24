8d8dddfe159e0094a9e514959a6e305826c71ab7
Justin Frydman <justin.frydman@gmail.com>
2021-06-23 22:19:44 -0600

    Bump to 2.1.1

--- core.php
+++ core.php
@@ -7,7 +7,7 @@
  * Author:             Modern Tribe
  * Author URI:         https://tri.be
  * Text Domain:        tribe-storage
- * Version:            2.1.0
+ * Version:            2.1.1
  * Requires at least:  5.6
  * Requires PHP:       7.3
  */
