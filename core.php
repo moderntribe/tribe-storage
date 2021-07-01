28d0d69b22ed344b6c1e1b3be041633da408db62
Justin Frydman <justin.frydman@gmail.com>
2021-07-01 14:27:03 -0600

    Fix plugins not having access to the loader

--- core.php
+++ core.php
@@ -7,7 +7,7 @@
  * Author:             Modern Tribe
  * Author URI:         https://tri.be
  * Text Domain:        tribe-storage
- * Version:            2.2.0
+ * Version:            2.2.1
  * Requires at least:  5.6
  * Requires PHP:       7.3
  */
---
+++ plugin-loader.php
@@ -0,0 +1,4 @@
+<?php declare(strict_types=1);
+
+// Load the stand alone plugin loader before anything else
+require_once __DIR__ . '/src/Plugin/Plugin_Loader.php';
