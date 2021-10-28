bf0c325add47faa5cba75840aa7156c70b59d57b
Justin Frydman <justin.frydman@gmail.com>
2021-10-28 14:25:33 -0600

    Fix typo

--- plugin-loader.php
+++ plugin-loader.php
@@ -1,4 +1,4 @@
 <?php declare(strict_types=1);
 
-// Load the stand alone plugin loader before anything else
+// Load the standalone plugin loader before anything else
 require_once __DIR__ . '/src/Plugin/Plugin_Loader.php';
