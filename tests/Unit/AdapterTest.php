429ff78d2abdcf53c9953c3117771b5ab953b43e
Justin <defunctl@users.noreply.github.com>
2021-10-28 14:24:03 -0600

    Better local adapter fallback handling (#14)
    * add nunomaduro/collision
    
    * use collision tests printer
    
    * Add __() mock
    
    * Refactor local adapter fallback, update and add tests
    
    * Pass the original upload_dir to the tribe/storage/attachment_url filter

--- tests/Unit/AdapterTest.php
+++ tests/Unit/AdapterTest.php
@@ -3,10 +3,13 @@
 namespace Tribe\Storage\Tests\Unit;
 
 use League\Flysystem\Adapter\Local;
+use League\Flysystem\Adapter\NullAdapter;
 use League\Flysystem\AdapterInterface;
 use League\Flysystem\Cached\CachedAdapter;
 use phpmock\mockery\PHPMockery;
 use Tribe\Storage\Tests\TestCase;
+
+use Brain\Monkey\Filters;
 
 /**
  * @runTestsInSeparateProcesses
@@ -41,6 +44,18 @@ class AdapterTest extends TestCase {
 		] );
 
 		require_once dirname( __DIR__ ) . '/../core.php';
+
+		PHPMockery::mock( 'Tribe\Storage', 'is_multisite' )
+		          ->once()
+		          ->andReturnFalse();
+
+		PHPMockery::mock( 'Tribe\Storage\Adapters\Cached_Adapters', 'get_transient' )
+		          ->once()
+		          ->andReturnFalse();
+
+		PHPMockery::mock( 'Tribe\Storage\Adapters\Cached_Adapters', 'wp_using_ext_object_cache' )
+		          ->twice()
+		          ->andReturnTrue();
 	}
 
 	protected function tearDown(): void {
@@ -50,30 +65,55 @@ class AdapterTest extends TestCase {
 	}
 
 	/**
+	 * Test the user can override the adapter with a filter.
+	 *
+	 * @throws \Exception
+	 */
+	public function test_developers_can_set_a_custom_adapter_with_filters(): void {
+		$null_adapter = new NullAdapter();
+
+		Filters\expectApplied( 'tribe/storage/flysystem_adapter' )
+			->once()
+			->andReturn( $null_adapter );
+
+		$container = tribe_storage()->container();
+		$adapter   = $container->get( AdapterInterface::class );
+
+		$this->assertInstanceOf( CachedAdapter::class, $adapter );
+		$this->assertInstanceOf( NullAdapter::class, $adapter->getAdapter() );
+	}
+
+	/**
 	 * If a user didn't define the required defines, this should fall back to the default, local adapter.
 	 *
 	 * @throws \Exception
 	 */
-	public function test_it_gets_a_local_adapter_with_missing_adapters(): void {
-
-		PHPMockery::mock( 'Tribe\Storage', 'is_multisite' )
-				  ->once()
-				  ->andReturnFalse();
-
-		PHPMockery::mock( 'Tribe\Storage\Adapters\Cached_Adapters', 'get_transient' )
-				  ->once()
-				  ->andReturnFalse();
-
-		PHPMockery::mock( 'Tribe\Storage\Adapters\Cached_Adapters', 'wp_using_ext_object_cache' )
-				  ->twice()
-				  ->andReturnTrue();
-
+	public function test_it_gets_a_local_adapter_with_no_defined_adapter(): void {
 		$container = tribe_storage()->container();
-
-		$adapter = $container->get( AdapterInterface::class );
+		$adapter   = $container->get( AdapterInterface::class );
 
 		$this->assertInstanceOf( CachedAdapter::class, $adapter );
 		$this->assertInstanceOf( Local::class, $adapter->getAdapter() );
+		$this->assertSame( 1, Filters\applied( 'tribe/storage/undefined_adapter_message' ) );
+		$this->assertSame( '1', getenv( 'MAGICK_THREAD_LIMIT' ) );
+	}
+
+	/**
+	 * Test it falls back to the local adapter if the user supplied an invalid adapter.
+	 *
+	 * @throws \Exception
+	 */
+	public function test_it_falls_back_to_local_adapter_with_invalid_custom_adapter(): void {
+		Filters\expectApplied( 'tribe/storage/flysystem_adapter' )
+			->once()
+			->andReturn( 'invalid' );
+
+		$container = tribe_storage()->container();
+		$adapter   = $container->get( AdapterInterface::class );
+
+		$this->assertInstanceOf( CachedAdapter::class, $adapter );
+		$this->assertInstanceOf( Local::class, $adapter->getAdapter() );
+		$this->assertSame( 1, Filters\applied( 'tribe/storage/undefined_adapter_message' ) );
 	}
 
 }
